7bbcfc8894cb34ad37ee013840c9182f8722e613
migueldxl <12401480@aluno.cotemig.com.br>
2026-05-20 16:30:38 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,17 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/usuario')
+def usuario():
+    return render_template('usuario.html', nome="Carlos", idade=28)
+
+@app.route('/perfil')
+def perfil():
+    usuario_dados = {"nome": "Ana", "email": "ana@email.com"}
+    return render_template('perfil.html', usuario=usuario_dados)
+
+@app.route('/alunos')
+def lista_alunos():
+    lista_nomes = ["Bruno", "Ana", "Diego", "Amanda"]
+    nota_aluno = 7.5
+    
+    return render_template(
+        'alunos.html', 
+        alunos=lista_nomes, 
+        nota=nota_aluno
+    )
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/profissional')
-def profissional():
-    return render_template('profissional.html')
-
-@app.route('/expectativas')
-def expectativas():
-    return render_template('expectativas.html')
-
+def home():
+    return "<h1>Bem-vindo! Escolha uma rota: /usuario, /perfil ou /alunos</h1>"
 if __name__ == '__main__':
     app.run(debug=True)
